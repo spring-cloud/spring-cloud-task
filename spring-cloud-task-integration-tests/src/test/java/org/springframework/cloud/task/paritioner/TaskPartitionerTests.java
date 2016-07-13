1e1f75d7bad171e9a41a79ee1e661c47dabd8d42
Glenn Renfro <grenfro@pivotal.io>
2016-07-13 14:22:23 -0400

    Applied the TaskPartitioner Integration tests.
    resolves #178
    Using task explorer and cleaned up the BatchExecution Event Tests
    
    Added spring-cloud-task-dependencies to dependency-management

--- spring-cloud-task-integration-tests/src/test/java/org/springframework/cloud/task/paritioner/TaskPartitionerTests.java
+++ spring-cloud-task-integration-tests/src/test/java/org/springframework/cloud/task/paritioner/TaskPartitionerTests.java
@@ -14,30 +14,20 @@
  * limitations under the License.
  */
 
-package org.springframework.cloud.task.launcher;
+package org.springframework.cloud.task.paritioner;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 import javax.sql.DataSource;
 
+import io.spring.PartitionedBatchJobApplication;
 import org.h2.tools.Server;
 import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.cloud.deployer.spi.local.LocalDeployerProperties;
-import org.springframework.cloud.deployer.spi.local.LocalTaskLauncher;
-import org.springframework.cloud.deployer.spi.task.TaskLauncher;
-import org.springframework.cloud.stream.annotation.Bindings;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.test.junit.rabbit.RabbitTestSupport;
-import org.springframework.cloud.task.launcher.util.TaskLauncherSinkApplication;
 import org.springframework.cloud.task.repository.TaskExecution;
 import org.springframework.cloud.task.repository.TaskExplorer;
 import org.springframework.cloud.task.repository.support.SimpleTaskExplorer;
@@ -48,26 +38,30 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.SocketUtils;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {TaskLauncherSinkApplication.class, TaskLauncherSinkTests.TaskLauncherConfiguration.class})
-public class TaskLauncherSinkTests {
+@SpringApplicationConfiguration(classes = {TaskPartitionerTests.TaskLauncherConfiguration.class})
+public class TaskPartitionerTests {
 
-	private final static int WAIT_INTERVAL = 500;
-	private final static int MAX_WAIT_TIME = 5000;
-	private final static String URL = "maven://org.springframework.cloud.task.app:"
-			+ "timestamp-task:jar:1.0.0.BUILD-SNAPSHOT";
-	private final static String DATASOURCE_URL;
+	private static String DATASOURCE_URL;
 	private final static String DATASOURCE_USER_NAME = "SA";
 	private final static String DATASOURCE_USER_PASSWORD = "";
 	private final static String DATASOURCE_DRIVER_CLASS_NAME = "org.h2.Driver";
-	private final static String TASK_NAME = "TASK_LAUNCHER_SINK_TEST";
+
+	private TaskExplorer taskExplorer;
+
+	@Autowired
+
+	private DataSource dataSource;
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		taskExplorer = new SimpleTaskExplorer(new TaskExecutionDaoFactoryBean(dataSource));
+	}
 
 	private static int randomPort;
 
@@ -77,34 +71,8 @@ public class TaskLauncherSinkTests {
 				+ "DB_CLOSE_ON_EXIT=FALSE";
 	}
 
-	@ClassRule
-	public static RabbitTestSupport rabbitTestSupport = new RabbitTestSupport();
-
-	@Autowired
-	@Bindings(TaskLauncherSink.class)
-	private Sink sink;
-
-	private DataSource dataSource;
-
-	private Map<String, String> properties;
-
-	private TaskExplorer taskExplorer;
-
-	@Autowired
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-		taskExplorer = new SimpleTaskExplorer(new TaskExecutionDaoFactoryBean(dataSource));
-	}
-
 	@Before
 	public void setup() {
-		properties = new HashMap<>();
-		properties.put("spring.datasource.url", DATASOURCE_URL);
-		properties.put("spring.datasource.username", DATASOURCE_USER_NAME);
-		properties.put("spring.datasource.password", DATASOURCE_USER_PASSWORD);
-		properties.put("spring.datasource.driverClassName", DATASOURCE_DRIVER_CLASS_NAME);
-		properties.put("spring.application.name",TASK_NAME);
-
 		JdbcTemplate template = new JdbcTemplate(this.dataSource);
 		template.execute("DROP TABLE IF EXISTS TASK_TASK_BATCH");
 		template.execute("DROP TABLE IF EXISTS TASK_SEQ");
@@ -123,56 +91,26 @@ public class TaskLauncherSinkTests {
 
 	@Test
 	public void testWithLocalDeployer() throws Exception {
-		launchTask(URL);
-		assertTrue(waitForDBToBePopulated());
+		SpringApplication app = new SpringApplication(PartitionedBatchJobApplication.class);
+		app.setAdditionalProfiles("master");
+		app.run("--server.port=0", "--spring.datasource.url=" + DATASOURCE_URL,
+						"--spring.datasource.username=" + DATASOURCE_USER_NAME,
+						"--spring.datasource.driverClassName=" + DATASOURCE_DRIVER_CLASS_NAME);
 
 		Page<TaskExecution> taskExecutions = taskExplorer.findAll(new PageRequest(0, 10));
-		TaskExecution te = taskExecutions.iterator().next();
-		assertEquals("Only one row is expected", 1, taskExecutions.getTotalElements());
-		assertEquals("return code should be 0", 0, taskExecutions.iterator().next().getExitCode().intValue());
-	}
-
-	private boolean tableExists() throws SQLException {
-		boolean result;
-		try (
-				Connection conn = dataSource.getConnection();
-				ResultSet res = conn.getMetaData().getTables(null, null, "TASK_EXECUTION",
-						new String[]{"TABLE"})) {
-			result = res.next();
+		assertEquals("Five rows are expected", 5, taskExecutions.getTotalElements());
+		assertEquals("Only One master is expected", 1, taskExplorer.getTaskExecutionCountByTaskName("batchEvents:master:0"));
+		assertEquals("4 partitions is expected", 4, taskExplorer.getTaskExecutionCountByTaskName("batchEvents:worker:0"));
+		for (TaskExecution taskExecution : taskExecutions) {
+			assertEquals("return code should be 0", 0, taskExecution.getExitCode().intValue());
 		}
-		return result;
-	}
-
-	private boolean waitForDBToBePopulated() throws Exception {
-		boolean isDbPopulated = false;
-		for (int waitTime = 0; waitTime <= MAX_WAIT_TIME; waitTime += WAIT_INTERVAL) {
-			Thread.sleep(WAIT_INTERVAL);
-			if (tableExists() && taskExplorer.getTaskExecutionCount() > 0) {
-				isDbPopulated = true;
-				break;
-			}
-		}
-		return isDbPopulated;
-	}
-
-	private void launchTask(String artifactURL) {
-		TaskLaunchRequest request = new TaskLaunchRequest(artifactURL, null, this.properties, null);
-		GenericMessage<TaskLaunchRequest> message = new GenericMessage<>(request);
-		this.sink.input().send(message);
 	}
 
 	@Configuration
 	public static class TaskLauncherConfiguration {
-		@Bean
-		public TaskLauncher taskLauncher() {
-			LocalDeployerProperties props = new LocalDeployerProperties();
-			props.setDeleteFilesOnExit(false);
-
-			return new LocalTaskLauncher(props);
-		}
 
 		@Bean(destroyMethod = "stop")
-		public Server initH2TCPServer() {
+		public org.h2.tools.Server initH2TCPServer() {
 			Server server;
 			try {
 				server = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort",
