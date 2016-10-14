81f34d118a4883188bc443b33fb17c538089a029
Glenn Renfro <grenfro@pivotal.io>
2016-10-14 17:34:35 -0400

    Adding comment for those upgrading from .m1.
    Removed column keyword from some of the definitions

--- spring-cloud-task-core/src/main/resources/org/springframework/cloud/task/migration/migration-hsqldb.sql
+++ spring-cloud-task-core/src/main/resources/org/springframework/cloud/task/migration/migration-hsqldb.sql
@@ -1,2 +1,4 @@
-alter table task_execution add column ERROR_MESSAGE VARCHAR(2500);
+/* If migrating from 1.1.0.M1 to 1.1.0.M2 you do not need to add
+the ERROR_MESSAGE column. */
+alter table task_execution add ERROR_MESSAGE VARCHAR(2500);
 alter table task_execution add EXTERNAL_EXECUTION_ID VARCHAR(255);
--- spring-cloud-task-core/src/main/resources/org/springframework/cloud/task/migration/migration-oracle.sql
+++ spring-cloud-task-core/src/main/resources/org/springframework/cloud/task/migration/migration-oracle.sql
@@ -1,3 +1,5 @@
+/* If migrating from 1.1.0.M1 to 1.1.0.M2 you do not need to add
+the ERROR_MESSAGE column. */
 alter table task_execution add ERROR_MESSAGE varchar2(2500);
 alter table task_execution add EXTERNAL_EXECUTION_ID varchar2(255);
 
