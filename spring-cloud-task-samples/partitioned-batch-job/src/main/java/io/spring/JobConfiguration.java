3df7995a3718aebb92dbb51fcc2a1c617bc3fff6
Glenn Renfro <grenfro@pivotal.io>
2017-01-10 13:41:09 -0500

    Updated Deployer Dependencies
    Updated Deployer Dependencies from 1.1.1.BUILD-SNAPSHOT to 1.1.2.RELEASE

--- spring-cloud-task-samples/partitioned-batch-job/src/main/java/io/spring/JobConfiguration.java
+++ spring-cloud-task-samples/partitioned-batch-job/src/main/java/io/spring/JobConfiguration.java
@@ -93,7 +93,7 @@ public class JobConfiguration {
 
 	@Bean
 	public PartitionHandler partitionHandler(TaskLauncher taskLauncher, JobExplorer jobExplorer) throws Exception {
-		Resource resource = resourceLoader.getResource("maven://io.spring.cloud:partitioned-batch-job:1.1.1.RELEASE");
+		Resource resource = resourceLoader.getResource("maven://io.spring.cloud:partitioned-batch-job:1.1.2.RELEASE");
 
 		DeployerPartitionHandler partitionHandler = new DeployerPartitionHandler(taskLauncher, jobExplorer, resource, "workerStep");
 
