0ed42bb87ce62f3a1b2554c403cd60e4cb1216b8
Glenn Renfro <grenfro@pivotal.io>
2018-01-22 10:53:36 -0500

    Allows failed batch apps to set exit code of task app.
    resolves #201
     Please enter the commit message for your changes. Lines starting
    
    Cleanup Removed taskProperties from TaskBatchAutoConfiguration

---
+++ spring-cloud-task-batch/src/main/java/org/springframework/cloud/task/batch/configuration/TaskJobLauncherAutoConfiguration.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright 2018 the original author or authors.
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.springframework.cloud.task.batch.configuration;
+
+import java.util.List;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Provides auto configuration for the
+ * {@link org.springframework.cloud.task.batch.handler.TaskJobLauncherCommandLineRunner}.
+ *
+ * @author Glenn Renfro
+ */
+@Configuration
+@ConditionalOnMissingBean(name = "taskJobLauncherCommandLineRunner")
+@ConditionalOnProperty(name = "spring.cloud.task.batch.command.line.runner.enabled", havingValue = "true", matchIfMissing = false)
+@EnableConfigurationProperties(TaskBatchProperties.class)
+public class TaskJobLauncherAutoConfiguration {
+
+	@Autowired
+	private TaskBatchProperties properties;
+
+	@Bean
+	public TaskJobLauncherCommandLineRunnerFactoryBean jobLauncherCommandLineRunner(JobLauncher jobLauncher,
+			JobExplorer jobExplorer, List<Job> jobs, JobRegistry jobRegistry) {
+		TaskJobLauncherCommandLineRunnerFactoryBean taskJobLauncherCommandLineRunner = new TaskJobLauncherCommandLineRunnerFactoryBean(
+				jobLauncher, jobExplorer, jobs, this.properties.getJobNames(), jobRegistry);
+		return taskJobLauncherCommandLineRunner;
+	}
+}
---
+++ spring-cloud-task-batch/src/test/java/org/springframework/cloud/task/batch/configuration/TaskJobLauncherAutoConfigurationTests.java
@@ -0,0 +1,89 @@
+/*
+ *  Copyright 2018 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.springframework.cloud.task.batch.configuration;
+
+import org.junit.After;
+import org.junit.Test;
+
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.autoconfigure.batch.JobLauncherCommandLineRunner;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.EmbeddedDataSourceConfiguration;
+import org.springframework.cloud.task.batch.handler.TaskJobLauncherCommandLineRunner;
+import org.springframework.cloud.task.batch.listener.TaskBatchExecutionListenerTests;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Glenn Renfro
+ */
+public class TaskJobLauncherAutoConfigurationTests {
+
+	private ConfigurableApplicationContext applicationContext;
+
+	@After
+	public void tearDown() {
+		if(this.applicationContext != null) {
+			this.applicationContext.close();
+		}
+	}
+
+	@Test
+	public void testAutobuiltDataSourceWithTaskJobLauncherCLR() {
+		String [] enabledArgs = new String[] {
+				"--spring.cloud.task.batch.command.line.runner.enabled=true",
+				"--spring.cloud.task.closecontext_enable=false"};
+		this.applicationContext = SpringApplication.run(new Class[]
+				{TaskBatchExecutionListenerTests.JobConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class,
+				EmbeddedDataSourceConfiguration.class,
+				BatchAutoConfiguration.class,
+				TaskJobLauncherAutoConfiguration.class}, enabledArgs);
+		assertThat(this.applicationContext.getBean(TaskJobLauncherCommandLineRunner.class)).isNotNull();
+		boolean exceptionThrown = false;
+		try {
+			this.applicationContext.getBean(JobLauncherCommandLineRunner.class);
+		}
+		catch(NoSuchBeanDefinitionException exception) {
+			exceptionThrown = true;
+		}
+		assertThat(exceptionThrown).isTrue();
+	}
+
+	@Test
+	public void testAutobuiltDataSourceWithTaskJobLauncherCLRDisabled() {
+		String [] enabledArgs = new String[] {"--spring.cloud.task.closecontext_enable=false"};
+		this.applicationContext = SpringApplication.run(new Class[]
+				{TaskBatchExecutionListenerTests.JobConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class,
+				EmbeddedDataSourceConfiguration.class,
+				BatchAutoConfiguration.class,
+				TaskJobLauncherAutoConfiguration.class}, enabledArgs);
+		assertThat(this.applicationContext.getBean(JobLauncherCommandLineRunner.class)).isNotNull();
+		boolean exceptionThrown = false;
+		try {
+			this.applicationContext.getBean(TaskJobLauncherCommandLineRunner.class);
+		}
+		catch(NoSuchBeanDefinitionException exception) {
+			exceptionThrown = true;
+		}
+		assertThat(exceptionThrown).isTrue();
+	}
+}
