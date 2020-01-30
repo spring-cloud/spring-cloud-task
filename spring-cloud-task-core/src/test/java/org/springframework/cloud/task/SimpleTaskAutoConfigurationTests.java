91084d96a4919a8976d271d67854f9745857cf37
Glenn Renfro <grenfro@pivotal.io>
2020-01-30 14:02:35 -0500

    Updated Versions & resolved failed tests

--- spring-cloud-task-core/src/test/java/org/springframework/cloud/task/SimpleTaskAutoConfigurationTests.java
+++ spring-cloud-task-core/src/test/java/org/springframework/cloud/task/SimpleTaskAutoConfigurationTests.java
@@ -130,9 +130,9 @@ public class SimpleTaskAutoConfigurationTests {
 				"bean 'taskLifecycleListener'; nested exception is " +
 				"org.springframework.dao.DataAccessResourceFailureException: " +
 				"Could not obtain sequence value; nested exception is org.h2.jdbc.JdbcSQLSyntaxErrorException: " +
-				"Syntax error in SQL statement \"SELECT FOOBARLESSSEQ.NEXTVAL FROM[*] DUAL \"; " +
+				"Syntax error in SQL statement \"SELECT FOOBARLESSSEQ.NEXTVAL FROM[*] DUAL\"; " +
 				"expected \"identifier\"; SQL statement:\n" +
-				"select foobarlessSEQ.nextval from dual [42001-199]", applicationContextRunner);
+				"select foobarlessSEQ.nextval from dual [42001-200]", applicationContextRunner);
 	}
 
 	@Test
