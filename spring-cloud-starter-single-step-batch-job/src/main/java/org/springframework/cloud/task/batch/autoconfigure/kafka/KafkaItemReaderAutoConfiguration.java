a84150860449b11362f0a8656ff6f4b5a8c46380
Glenn Renfro <grenfro@pivotal.io>
2020-06-23 09:45:18 -0400

    Introducing KafkaItemReader for single step jobs
    resolves #679
    
    Rebase update
    
    rebase update pom cleanup
    
    Cleaned up tests
    
    Updated
    
    Added updates based on code review
    
    polish

---
+++ spring-cloud-starter-single-step-batch-job/src/main/java/org/springframework/cloud/task/batch/autoconfigure/kafka/KafkaItemReaderAutoConfiguration.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.task.batch.autoconfigure.kafka;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Properties;
+
+import org.springframework.batch.item.kafka.KafkaItemReader;
+import org.springframework.batch.item.kafka.builder.KafkaItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+/**
+ *
+ * AutoConfiguration for a {@code KafkaItemReader}.
+ *
+ * @author Glenn Renfro
+ * @since 2.3
+ */
+@Configuration
+@EnableConfigurationProperties({ KafkaProperties.class, KafkaItemReaderProperties.class })
+@AutoConfigureAfter(BatchAutoConfiguration.class)
+public class KafkaItemReaderAutoConfiguration {
+
+	@Autowired
+	private KafkaProperties kafkaProperties;
+
+	@Bean
+	@ConditionalOnMissingBean
+	@ConditionalOnProperty(prefix = "spring.batch.job.kafkaitemreader", name = "name")
+	public KafkaItemReader<Object, Map<Object, Object>> kafkaItemReader(
+			KafkaItemReaderProperties kafkaItemReaderProperties) {
+		Properties consumerProperties = new Properties();
+		consumerProperties.putAll(this.kafkaProperties.getConsumer().buildProperties());
+		validateProperties(kafkaItemReaderProperties);
+		if (kafkaItemReaderProperties.getPartitions() == null
+				|| kafkaItemReaderProperties.getPartitions().size() == 0) {
+			kafkaItemReaderProperties.setPartitions(new ArrayList<>(1));
+			kafkaItemReaderProperties.getPartitions().add(0);
+		}
+		return new KafkaItemReaderBuilder<Object, Map<Object, Object>>()
+				.partitions(kafkaItemReaderProperties.getPartitions())
+				.consumerProperties(consumerProperties)
+				.name(kafkaItemReaderProperties.getName())
+				.pollTimeout(Duration
+						.ofSeconds(kafkaItemReaderProperties.getPollTimeOutInSeconds()))
+				.saveState(kafkaItemReaderProperties.isSaveState()).topic(kafkaItemReaderProperties.getTopic()).build();
+	}
+
+	private void validateProperties(KafkaItemReaderProperties kafkaItemReaderProperties) {
+		if (!StringUtils.hasText(kafkaItemReaderProperties.getName())) {
+			throw new IllegalArgumentException("Name must not be empty or null");
+		}
+		if (!StringUtils.hasText(kafkaItemReaderProperties.getTopic())) {
+			throw new IllegalArgumentException("Topic must not be empty or null");
+		}
+		if (!StringUtils.hasText(this.kafkaProperties.getConsumer().getGroupId())) {
+			throw new IllegalArgumentException("GroupId must not be empty or null");
+		}
+		if (this.kafkaProperties.getBootstrapServers() == null
+				|| this.kafkaProperties.getBootstrapServers().size() == 0) {
+			throw new IllegalArgumentException("Bootstrap Servers must be configured");
+		}
+	}
+
+}
---
+++ spring-cloud-starter-single-step-batch-job/src/main/java/org/springframework/cloud/task/batch/autoconfigure/kafka/KafkaItemReaderProperties.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.task.batch.autoconfigure.kafka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties to configure a {@code KafkaItemReader}.
+ *
+ * @author Glenn Renfro
+ * @since 2.3
+ */
+@ConfigurationProperties(prefix = "spring.batch.job.kafkaitemreader")
+public class KafkaItemReaderProperties {
+
+	private String name;
+
+	private String topic;
+
+	private List<Integer> partitions = new ArrayList<>();
+
+	private long pollTimeOutInSeconds = 30L;
+
+	private boolean saveState = true;
+
+	/**
+	 * Returns the configured value of the name used to calculate {@code ExecutionContext}
+	 * keys.
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * The name used to calculate the key within the
+	 * {@link org.springframework.batch.item.ExecutionContext}.
+	 * @param name name of the writer instance
+	 * @see org.springframework.batch.item.ItemStreamSupport#setName(String)
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Returns the name of the topic from which messages will be read.
+	 * @return the name of the topic.
+	 */
+	public String getTopic() {
+		return topic;
+	}
+
+	/**
+	 * The topic name from which the messages will be read.
+	 * @param topic name of the topic
+	 */
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	/**
+	 * A list of partitions to manually assign to the consumer. Defaults to a single entry
+	 * value of 1.
+	 * @return the list of partitions.
+	 */
+	public List<Integer> getPartitions() {
+		return partitions;
+	}
+
+	/**
+	 * A list of partitions to manually assign to the consumer. Defaults to a single entry
+	 * value of 1.
+	 * @param partitions list of partitions
+	 */
+	public void setPartitions(List<Integer> partitions) {
+		this.partitions = partitions;
+	}
+
+	/**
+	 * Get the pollTimeout for the poll() operations. Defaults to 30 seconds.
+	 * @return long containing the poll timeout.
+	 */
+	public long getPollTimeOutInSeconds() {
+		return pollTimeOutInSeconds;
+	}
+
+	/**
+	 * Set the pollTimeout for the poll() operations. Defaults to 30 seconds.
+	 * @param pollTimeOutInSeconds the number of seconds to wait before timing out.
+	 */
+	public void setPollTimeOutInSeconds(long pollTimeOutInSeconds) {
+		this.pollTimeOutInSeconds = pollTimeOutInSeconds;
+	}
+	/**
+	 * Configure if the state of the {@link org.springframework.batch.item.ItemStreamSupport}
+	 * should be persisted within the {@link org.springframework.batch.item.ExecutionContext}
+	 * for restart purposes.  Defaults to true.
+	 * @return current status of the saveState flag.
+	 */
+	public boolean isSaveState() {
+		return saveState;
+	}
+
+	/**
+	 * Configure if the state of the {@link org.springframework.batch.item.ItemStreamSupport}
+	 * should be persisted within the {@link org.springframework.batch.item.ExecutionContext}
+	 * for restart purposes.
+	 * @param saveState true if state should be persisted.  Defaults to true.
+	 */
+	public void setSaveState(boolean saveState) {
+		this.saveState = saveState;
+	}
+}
