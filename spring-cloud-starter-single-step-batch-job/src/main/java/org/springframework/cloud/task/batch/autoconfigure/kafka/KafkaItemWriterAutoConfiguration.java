626af448289945e4037b62f823f1041f467ca981
Glenn Renfro <grenfro@pivotal.io>
2020-08-03 16:27:33 -0400

    Adding KafkaItemWriter single step autoconfig
    resolves #696
    
    Updated to allow user to set the itemKeyMapper
    
    Set a very simple keymapper that returns the same result
    
    Updated based on code reviews
    
    Updated Kafka version

---
+++ spring-cloud-starter-single-step-batch-job/src/main/java/org/springframework/cloud/task/batch/autoconfigure/kafka/KafkaItemWriterAutoConfiguration.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.task.batch.autoconfigure.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.batch.item.kafka.KafkaItemWriter;
+import org.springframework.batch.item.kafka.builder.KafkaItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.util.Assert;
+
+/**
+ *
+ * Autconfiguration for a {@code KafkaItemReader}.
+ *
+ * @author Glenn Renfro
+ * @since 2.3
+ */
+@Configuration
+@EnableConfigurationProperties({ KafkaProperties.class, KafkaItemWriterProperties.class })
+@AutoConfigureAfter(BatchAutoConfiguration.class)
+public class KafkaItemWriterAutoConfiguration {
+
+	@Autowired
+	private KafkaProperties kafkaProperties;
+
+	@Bean
+	@ConditionalOnMissingBean
+	@ConditionalOnProperty(prefix = "spring.batch.job.kafkaitemwriter", name = "topic")
+	public KafkaItemWriter<Object, Map<Object, Object>> kafkaItemWriter(
+			KafkaItemWriterProperties kafkaItemWriterProperties,
+			ProducerFactory<Object, Map<Object, Object>> producerFactory,
+			@Qualifier("batchItemKeyMapper") Converter<Object, Object> itemKeyMapper) {
+
+		validateProperties(kafkaItemWriterProperties);
+		KafkaTemplate template = new KafkaTemplate(producerFactory);
+		template.setDefaultTopic(kafkaItemWriterProperties.getTopic());
+		return new KafkaItemWriterBuilder<Object, Map<Object, Object>>()
+				.delete(kafkaItemWriterProperties.isDelete()).kafkaTemplate(template)
+				.itemKeyMapper(itemKeyMapper).build();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(name = "batchItemKeyMapper")
+	public Converter<Object, Object> batchItemKeyMapper() {
+		return new Converter<Object, Object>() {
+			@Override
+			public Object convert(Object source) {
+				return source;
+			}
+		};
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
+	ProducerFactory<Object, Map<Object, Object>> producerFactory() {
+		Map<String, Object> configs = new HashMap<>();
+		configs.putAll(this.kafkaProperties.getProducer().buildProperties());
+		return new DefaultKafkaProducerFactory<Object, Map<Object, Object>>(configs, null,
+				new JsonSerializer<>());
+	}
+
+	private void validateProperties(KafkaItemWriterProperties kafkaItemWriterProperties) {
+		Assert.hasText(kafkaItemWriterProperties.getTopic(),
+				"topic must not be empty or null");
+	}
+
+}
