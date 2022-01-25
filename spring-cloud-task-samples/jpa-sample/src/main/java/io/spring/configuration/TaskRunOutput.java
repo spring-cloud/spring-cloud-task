c753b98a767419d75ac1faf0f1e50fe538f9596e
Glenn Renfro <grenfro@vmware.com>
2022-01-25 14:17:57 -0500

    Update task to use Milestone build
    Updated partitioned sample readme
    
    Updated samples base boot version
    
    Updated jpa sample to use jakarta

--- spring-cloud-task-samples/jpa-sample/src/main/java/io/spring/configuration/TaskRunOutput.java
+++ spring-cloud-task-samples/jpa-sample/src/main/java/io/spring/configuration/TaskRunOutput.java
@@ -16,11 +16,11 @@
 
 package io.spring.configuration;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * Entity for the id and output to be written to the data store.
