e4581420b04e872f8aac4cb3ad8bab62bbe9c941
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2022-07-22 17:55:02 +0200

    Allow users to select the transaction manager to use in TaskRepository
    * Update `@Transactional` attribute to use `spring.cloud.task.transaction-manager` property
    * Make `springCloudTaskTransactionManager` bean definition conditional on a missing property
    
    Issue #769

--- spring-cloud-task-core/src/main/java/org/springframework/cloud/task/configuration/SingleInstanceTaskListener.java
+++ spring-cloud-task-core/src/main/java/org/springframework/cloud/task/configuration/SingleInstanceTaskListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 the original author or authors.
+ * Copyright 2018-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,6 +47,7 @@ import org.springframework.transaction.PlatformTransactionManager;
  * it should be added as the first listener in the chain.
  *
  * @author Glenn Renfro
+ * @author Mahmoud Ben Hassine
  * @since 2.0.0
  */
 public class SingleInstanceTaskListener implements ApplicationListener<ApplicationEvent> {
