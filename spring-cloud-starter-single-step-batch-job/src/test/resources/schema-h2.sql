9cd844827c20b5b761ef5790159ad8c416069315
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2023-09-08 16:32:26 +0200

    Initial commit

--- spring-cloud-starter-single-step-batch-job/src/test/resources/schema-h2.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS item
-(
-   item_name varchar(55)
-);
--- spring-cloud-task-core/src/main/resources/org/springframework/cloud/task/migration/1.1.x/migration-mysql.sql
+++
@@ -1,5 +0,0 @@
-/* If migrating from 1.1.0.M1 to 1.1.0.RELEASE you do not need to add
-the ERROR_MESSAGE column. */
-alter table TASK_EXECUTION add ERROR_MESSAGE VARCHAR(2500);
-alter table TASK_EXECUTION add EXTERNAL_EXECUTION_ID VARCHAR(255);
-
